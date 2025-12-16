bf8fa8b0cc9bd9b9e0fc457baddef4570fea064b
Manjunathan-xecutables <manjunathan.xecutables@gmail.com>
2025-12-16 17:47:12 +0530

    fix(mongoService): correct collection access in search_by_id method

--- fabric/build/lib/database/mongoService.py
+++ fabric/build/lib/database/mongoService.py
@@ -20,7 +20,7 @@ class MongoService:
     def search_by_id(self, db_name, entityid, ids):
         db = self.client[db_name]
         collection_name = self.entityMapper(entityid)
-        collection = db.name[collection_name]
+        collection = db[collection_name]
         qry = {"_id": {"$in": ids}}
         col = collection.find(qry)
         results = list(col)
